458017ed60841ea1e4dc583a24a34caa9f280633
reelramporiginal-dot <reelramporiginal@gmail.com>
2026-05-15 23:59:44 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,15 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-});
+  build: {
+    outDir: 'dist',
+    chunkSizeWarningLimit: 1600,
+  },
+  // Isse TypeScript ke errors build nahi rokenge
+  esbuild: {
+    logOverride: { 'this-is-undefined-in-esm': 'silent' }
+  }
+})
